7ac23729d569a1784cd528b0258c814bf5a27403
zadragon <mase0804@naver.com>
2021-11-17 00:02:32 +0900

    update

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Home from "./routes/Home";
 import About from "./routes/About";
 import Detail from "./routes/Detail";
 import Navigation from "./components/Navigation";
-//import "./App.css";
 
 function App() {
     return (
@@ -12,7 +11,7 @@ function App() {
             <Navigation />
             <Route path="/" exact={true} component={Home} />
             <Route path="/about" component={About} />
-            <Route path="/movie-detail" component={Detail} />
+            <Route path="/movie/:id" component={Detail} />
         </HashRouter>
     );
 }
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,23 +1,22 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
-//import "./Movie.css";
 
-function Movie({ year, title, summary, poster, genres }) {
+function Movie({ id, year, title, summary, poster, genres }) {
     return (
-        <Link
-            to={{
-                pathname: "/movie-detail",
-                state: {
-                    year,
-                    title,
-                    summary,
-                    poster,
-                    genres
-                }
-            }}
-        >
-            <div className="movie">
+        <div className="movie">
+            <Link
+                to={{
+                    pathname: `/movie/${id}`,
+                    state: {
+                        year,
+                        title,
+                        summary,
+                        poster,
+                        genres
+                    }
+                }}
+            >
                 <img src={poster} alt={title} title={title} />
                 <div className="movie__data">
                     <h3 className="movie__title">{title}</h3>
@@ -31,8 +30,8 @@ function Movie({ year, title, summary, poster, genres }) {
                     </ul>
                     <p className="movie__summary">{summary.slice(0, 180)}...</p>
                 </div>
-            </div>
-        </Link>
+            </Link>
+        </div>
     );
 }
 
